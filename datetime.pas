8e0fc85c9a40ab78225b7ca817187353f2509ca7
Сергей Чикун <chikun@sfedu.ru>
2021-10-21 09:03:00 +0300

    add function SecondInMinute

--- datetime.pas
+++ datetime.pas
@@ -1,3 +1,13 @@
-﻿begin
+﻿function IsLeapYear(year: integer): boolean;
+begin
+  result := false;
+  if ((year mod 400) = 0) and ((year mod 100) <> 0) then result := true;
+  if ((year mod 400) = 0) and ((year mod 400) = 0) then result := true;
+end;
+function SecondInMinute(integer): integer;
+begin
+  result := 60;
+end;
+begin
   
 end.
