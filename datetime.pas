ef23fb45232fd630aad63932850fb5fb729bc6ee
Сергей Чикун <chikun@sfedu.ru>
2021-10-21 09:28:25 +0300

    add function LaterInYear

--- datetime.pas
+++ datetime.pas
@@ -1,3 +1,7 @@
 ﻿begin
-  
+ function IsLeapYear(year: integer): boolean;
+begin
+  if ((year mod 4) = 0) and ((year mod 100) <> 0) then result := true;
+  if ((year mod 4) = 0) and ((year mod 400) = 0) then result := true;
+end; 
 end.
