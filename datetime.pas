c625a49df51fb420c1c8c928535d4e414996068d
Сергей Чикун <chikun@sfedu.ru>
2021-10-21 09:13:45 +0300

    Revert "add function secondinminute"
    This reverts commit 4737004e1a57cbc30438472ea96ba035a2cad1e4.

--- datetime.pas
+++ datetime.pas
@@ -1,12 +1,3 @@
-﻿function IsLeapYear(year: integer): boolean;
-begin
-  if ((year mod 4) = 0) and ((year mod 100) <> 0) then result := true;
-  if ((year mod 4) = 0) and ((year mod 400) = 0) then result := true;
-end;
-function SecondInMinute(integer): integer;
-begin
-  result := 60;
-end;
-begin
+﻿begin
   
 end.
